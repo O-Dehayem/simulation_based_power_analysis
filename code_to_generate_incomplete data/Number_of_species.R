e234df45df972ef170b810e5923fd02cd9f48edb
Ornela Dehayem Nanwou <o.dehayem@gmail.com>
2025-05-02 09:23:56 +0200

    Update function description

--- code_to_generate_incomplete data/Number_of_species.R
+++ code_to_generate_incomplete data/Number_of_species.R
@@ -1,6 +1,6 @@
-#' The function takes a dataset as input and returns a list where each element corresponds to an island in the dataset.
-#' For each island, the function calculates the number of species by counting the number of branching times minus one for 
-#' lineages.
+#' This function takes a dataset as input and returns a list where each element corresponds to an island in the dataset.
+#' For each island, the function calculates the number of species by counting the branching times 
+#' of each lineage and subtracting one, as the first branching time represents the island's age.
 
 
 
